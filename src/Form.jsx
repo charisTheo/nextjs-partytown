08842bfc84df1055e6b3d6637c79f8acc76defcf
Harry Theo <harrytheo@google.com>
2022-07-20 15:25:22 +0100

    Added recaptcha script

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import { useState } from 'react';
+import Script from 'next/script'
 
 const FORM_DEFAULT_VALUES = {
     name: '',
@@ -12,14 +14,34 @@ export default function Form() {
         ...formValues,
         [e.target.name]: e.target.value
     });
+    
     const handleSubmit = (e) => {
         e.preventDefault();
+        const form = e.target
+
+        // This is the important part, test if form is valid
+        if (form.checkValidity() === false){
+            // This is the magic function that displays the validation errors to the user
+            return form.reportValidity();
+        }
+
         setFormValues(FORM_DEFAULT_VALUES)
         alert('Form submitted!')
     };
 
     return (
       <form onSubmit={handleSubmit} className='flex-column'>
+        <Head>
+            <script>
+               {`function recaptchaCallback () {
+                   const form = document.querySelector('form');
+                   const event = new Event('submit', { 'bubbles': true, 'cancelable': true });
+                   form.dispatchEvent(event);
+                }`}
+            </script>
+        </Head>
+        <Script src='https://www.google.com/recaptcha/api.js' />
+
         <label>
           Name
           <input type="text" value={formValues.name} name='name' onChange={handleChange} />
@@ -30,7 +52,13 @@ export default function Form() {
           <input type="email" value={formValues.email} name='email' onChange={handleChange} required />
         </label>
 
-        <button type="submit">Submit</button>
+        <button 
+          type="submit"
+          className="g-recaptcha" 
+          data-sitekey={process.env.NEXT_LOCAL_RECAPTCHA_SITE_KEY} 
+          data-callback='recaptchaCallback'
+          data-action='submit'
+        >Submit</button>
       </form>
     );
 }
